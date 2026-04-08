60013253646776133392eb8de749ce57748cf37d
Hourglass5177 <78329846+Hourglass5177@users.noreply.github.com>
2026-04-08 13:16:55 +0800

    完善地图模块

---
+++ 地图/市基类.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name 市基类
+@export var region: 非遗牌.REGION = 非遗牌.REGION.鄂州
+@export var num: int = 0
+@export var first_idx: int = 0
+@export var first_loc: Vector3i = Vector3i(0,0,0)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
